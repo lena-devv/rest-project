d6ea7cb1c98e56ef0098ef1e09b9673e32062f65
Elena_Nazarova <Elena_Nazarova@epam.com>
2019-12-26 14:12:12 +0300

    Created rest client

--- src/main/scala/com/epam/example/RestClient.scala
+++ src/main/scala/com/epam/example/RestClient.scala
@@ -1,24 +1,44 @@
 package com.epam.example
 
-import akka.actor.ActorSystem
+import scala.util.{ Failure, Success }
+import akka.actor.{Actor, ActorLogging, ActorSystem}
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.model.{ContentTypes, HttpEntity}
-import akka.stream.ActorMaterializer
+import akka.http.scaladsl.model.{ContentTypes, HttpEntity, HttpMethod, HttpMethods, HttpRequest, HttpResponse, StatusCodes}
+import akka.stream.{ActorMaterializer, ActorMaterializerSettings}
+import akka.pattern.pipe
+import akka.util.ByteString
 
+import scala.concurrent.{ExecutionContextExecutor, Future}
 import scala.io.StdIn
+import akka.http.scaladsl.model._
 
 object RestClient {
 
   def main(args: Array[String]): Unit = {
-    val client: RestClient = new RestClient()
-    client.call("")
+    implicit val system: ActorSystem = ActorSystem()
+    implicit val materializer: ActorMaterializer = ActorMaterializer()
+    implicit val executionContext: ExecutionContextExecutor = system.dispatcher
+
+    val responseFuture: Future[HttpResponse] = Http().singleRequest(HttpRequest(uri = "http://localhost:8080/event/1", method = HttpMethods.GET))
+    responseFuture.map {
+      case response @ HttpResponse(StatusCodes.OK, headers, entity, _) => {
+        val headersStr: String = headers.map(header => s"${header.name()}: ${header.value()}").toList.mkString("\n")
+        println(s"entity: $entity, headers: $headersStr")
+      }
+      case _ => sys.error("something wrong")
+    }
+    responseFuture.onComplete(done => {system.terminate()})
+    /*responseFuture.onComplete {
+      case Success(res) => println(res)
+      case Failure(_)   => sys.error("something wrong")
+    }*/
   }
 }
 
-class RestClient {
+/*object RestClient {
 
   def call(url: String): Unit = {
 
   }
 
-}
+}*/
