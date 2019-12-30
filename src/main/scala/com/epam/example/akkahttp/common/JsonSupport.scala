a4dc458eb37d7e4da29052432f8062815729dcbd
Elena_Nazarova <Elena_Nazarova@epam.com>
2019-12-30 16:09:42 +0300

    Refactored

--- src/main/scala/com/epam/example/akkahttp/common/JsonSupport.scala
+++ src/main/scala/com/epam/example/akkahttp/common/JsonSupport.scala
@@ -1,9 +1,9 @@
 package com.epam.example.akkahttp.common
 
 import de.heikoseeberger.akkahttpjson4s.Json4sSupport
-import org.json4s.{DefaultFormats, Formats, native}
+import org.json4s.{DefaultFormats, Formats, Serialization, native}
 
 trait JsonSupport extends Json4sSupport {
-    implicit val serialization = native.Serialization
+    implicit val serialization: Serialization = native.Serialization
     implicit def json4sFormats: Formats = DefaultFormats
 }
